8be5677aebfebcb7f2d58eda54154c44a5691e51
Owen Glahn <owen.glahn@mail.mcgill.ca>
2023-04-20 20:47:26 -0400

    Fix prompt

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	var reader bufio.Reader = *bufio.NewReader(os.Stdin)
 	for {
 		out, _ := exec.Command("pwd").Output()
-		fmt.Printf("\n %s :) ", strings.TrimSuffix(string(out), "\n"))
+		fmt.Printf("%s :) ", strings.TrimSuffix(string(out), "\n"))
 		input, _ := reader.ReadString('\n')
 		errs := utils.ParseCommandsSequential(input)
 		for _, err := range errs {
